e58e65198606d25c4d42321f0fa5cefed0e9abed
Marko Knöbl <marko.kn@gmail.com>
2023-05-02 20:40:00 +0200

    tasks for the day: map, filter, ...

--- by-date/2023-05-02-map-filter-reduce-employees/processEmployees.js
+++ by-date/2023-05-02-map-filter-reduce-employees/processEmployees.js
@@ -4,19 +4,30 @@ const fs = require("fs");
 // read and parse the JSON data
 const EMPLOYEES = JSON.parse(fs.readFileSync("./data.json"));
 
+// filter
 /**
  * Get employees whose position is "Director"
  */
-function getYoungDirectors(employees) {
-  // TODO
+function getDirectors(employees) {
+  return employees.filter(
+    (employee) => employee.position === "Director"
+  );
 }
-console.log(getYoungDirectors(EMPLOYEES));
+console.log(getDirectors(EMPLOYEES));
 
 /**
  * Get employee with age incremented by one
  */
 function getEmployeeOneYearOlder(employee) {
-  // TODO
+  const newEmployee = {
+    name: employee.name,
+    level: employee.level,
+    position: employee.position,
+    present: employee.present,
+    brand: employee.brand,
+    age: employee.age + 1
+  }
+  return newEmployee;
 }
 console.log(getEmployeeOneYearOlder(EMPLOYEES[0]));
 console.log(getEmployeeOneYearOlder(EMPLOYEES[1]));
@@ -25,7 +36,9 @@ console.log(getEmployeeOneYearOlder(EMPLOYEES[1]));
  * Get multiple employees with each age incremented by one
  */
 function getEmployeesOneYearOlder(employees) {
-  // TODO
+  return employees.map(
+    (employee) => getEmployeeOneYearOlder(employee)
+  );
 }
 console.log(getEmployeesOneYearOlder(EMPLOYEES));
 console.log(getEmployeesOneYearOlder(EMPLOYEES));
