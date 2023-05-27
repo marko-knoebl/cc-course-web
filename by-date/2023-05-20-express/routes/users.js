2a3155f7dccaa420b7044221349d00ee08634fe6
Marko Knöbl <marko.kn@gmail.com>
2023-05-27 11:21:02 +0200

    express users exercise

--- by-date/2023-05-20-express/routes/users.js
+++ by-date/2023-05-20-express/routes/users.js
@@ -1,26 +1,61 @@
-const { Router } = require("express");
+const express = require("express");
+const fs = require("fs");
 
-const userData = ["adrian", "agata", "marko"];
+const usersRouter = new express.Router();
 
-const usersRouter = new Router();
+function readUsers() {
+  const fileContent = fs.readFileSync("data/users.json", "utf-8");
+  const userData = JSON.parse(fileContent);
+  return userData;
+}
 
 // CRUD
-// - read
-// - create
-// - update
-// - delete
+// - read (GET)
+// - create (POST)
+// - update (PATCH, PUT)
+// - delete (DELETE)
 
 // read
 usersRouter.get("/", (req, res) => {
-  res.json(userData);
+  try {
+    // read json file
+    const userData = readUsers();
+    res.json(userData);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send();
+  }
+});
+
+usersRouter.get("/stats", (req, res) => {
+  try {
+    // read json file
+    const userData = readUsers();
+    res.json({ numUsers: userData.length, backLink: "http://localhost:3000/" });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send();
+  }
 });
 
 // create
 usersRouter.post("/", (req, res) => {
   // read data from the request (body)
   const newUser = req.body;
-  userData.push(newUser);
-  res.json({ status: "success" });
+
+  try {
+    const userData = readUsers();
+    userData.push(newUser);
+    const usersString = JSON.stringify(userData);
+    fs.writeFileSync("data/users.json", usersString);
+    res.json({ status: "success" });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+    res.send();
+  }
 });
 
 // // update
@@ -30,4 +65,5 @@ usersRouter.post("/", (req, res) => {
 // // delete
 // usersRouter.delete();
 
+// named export
 exports.usersRouter = usersRouter;
